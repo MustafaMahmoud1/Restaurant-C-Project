cbcbc7df012909de78619a5d815247e18ecd56ca
MustafaMahmoud1 <106413978+MustafaMahmoud1@users.noreply.github.com>
2024-01-01 15:35:33 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Restaurant_C__Project
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Numerics;
+
+namespace Restaurant_C__Project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your Username (Used when Logging in)");
+            string username = Console.ReadLine();
+            username = username.ToLower();
+
+            Console.WriteLine("Enter your Password");
+            string password = Console.ReadLine();
+            password = password.ToLower();
+
+            Console.WriteLine("Enter your Fullname");
+            string fullname = Console.ReadLine();
+            fullname = fullname.ToLower();
+
+            string phone = "";
+            while (true)
+            {
+                Console.WriteLine("Enter your Phone Number");
+                phone = Console.ReadLine();
+                if (phone.StartsWith("01") && phone.Length == 11 && phone.All(char.IsDigit))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter a valid phone number.");
+                }
+            }
+            Console.WriteLine("Enter your Address");
+            string address = Console.ReadLine();
+            address = address.ToLower();
+
+            Utilities.GetInstance().signUpUser(fullname, phone, address, username, password);
         }
     }
 }
