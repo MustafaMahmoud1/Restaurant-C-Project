ba03fc11afdbaeb598d410a329d20d80ca71fa12
MustafaMahmoud1 <106413978+MustafaMahmoud1@users.noreply.github.com>
2024-01-01 15:46:50 +0200

    test

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Restaurant_C__Project
+{
+    public class Utilities
+    {
+        private static Utilities util_instance;
+
+        private Utilities()
+        {
+        }
+
+        public static Utilities GetInstance()
+        {
+            if (util_instance == null)
+            {
+                util_instance = new Utilities();
+            }
+            return util_instance;
+        }
+
+        public void signUpUser(string fullname, string phone, string address, string username, string password)
+        {
+            Customer customer = new Customer(fullname, phone, address);
+            SystemUser user = new SystemUser(username, password, "customer");
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_C__Project
+{
+    internal class Customer
+    {
+        public string fullName { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public List<Order> activeOrders { get; set; }
+        public List<Order> orderHistory { get; set; }
+        public List<Reservation> activeReservations { get; set; }
+        public int balance { get; set; }
+
+        public Customer(string fullname, string phonenumber, string address)
+        {
+            List<Order> activeOrders = new List<Order>;
+            List<Order> orderHistory = new List<Order>;
+            List<Reservation> activeReservations = new List<Reservation>;
+            balance = 0;
+            fullName = fullname;
+            phoneNumber = phonenumber;
+            this.address = address;
+        }
+
+
+
+    }
+}
