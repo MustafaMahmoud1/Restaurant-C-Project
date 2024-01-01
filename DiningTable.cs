df5788c2304586655d4ca2a3e55844010f0ebf7b
bassantmagdy2 <155384109+bassantmagdy2@users.noreply.github.com>
2024-01-01 20:43:57 +0200

    Add files via upload

---
+++ DiningTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c__project
+{
+    internal class DiningTable
+    {
+        private int tableNo { get; set; }
+        private int tableCapicty { get; set; }
+        
+
+        private bool type { get; set; }
+
+        public DiningTable(int tableNo , int tableCapicty , bool state , bool type)
+        {
+            this.tableNo = tableNo;
+            this.tableCapicty = tableCapicty;
+            
+
+            this.type = type;
+        }
+
+    }
+}
