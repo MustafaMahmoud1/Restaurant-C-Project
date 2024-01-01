238bf65663a2f3df2f5ff83cb6687a6b19b45c09
Abdelrahman Shalaby <93157202+AShalabyElghoul@users.noreply.github.com>
2024-01-01 20:36:11 +0200

    menu.cs

--- menu.cs
+++ menu.cs
@@ -1,29 +1,91 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace round2
+namespace Restaurant_C__Project
 {
     public sealed class menu
     {
+
         List<Item> MenuItems = new List<Item>()
         {
             new Item
-            {ItemName="pasta" , ItemPrice=100 , ItemID=1 , description="errkfjnejnerjgnejgnerjfnqerjne", availability=true , category="main dish" ,ItemRecipe r1 =new Recipe() },
+            {
+                ItemName="Spaghetti Bolognese" ,
+                ItemPrice=100 ,
+                ItemID=1 ,
+                description="Classic Italian dish with meat sauce",
+                availability=true ,
+                category="pasta" ,
+                recipe=new List<ItemIngredient>
+                {
+                     new ItemIngredient { ItemIngredientID = 101, ItemIngredientQuantity = "500g" },
+                    new ItemIngredient { ItemIngredientID = 201, ItemIngredientQuantity  = "300g" },
+                    new ItemIngredient { ItemIngredientID = 301, ItemIngredientQuantity  = "1 cup" },
+                    new ItemIngredient { ItemIngredientID = 401, ItemIngredientQuantity  = "2 cloves" },
+                    new ItemIngredient { ItemIngredientID = 501, ItemIngredientQuantity  = "1 tsp" }
+                }
+            },
             new Item
-            {ItemName="pasta" , ItemPrice=100 , ItemID=1 , description="errkfjnejnerjgnejgnerjfnqerjne", availability=true , category="main dish" , ItemRecipe},
-             new Item
-            {ItemName="pasta" , ItemPrice=100 , ItemID=1 , description="errkfjnejnerjgnejgnerjfnqerjne", availability=true , category="main dish" , ItemRecipe},
-              new Item
-            {ItemName="pasta" , ItemPrice=100 , ItemID=1 , description="errkfjnejnerjgnejgnerjfnqerjne", availability=true , category="main dish" , ItemRecipe},
-               new Item
-            {ItemName="pasta" , ItemPrice=100 , ItemID=1 , description="errkfjnejnerjgnejgnerjfnqerjne", availability=true , category="main dish" , ItemRecipe},
+            {
+
+                ItemName = "Chicken Caesar Salad",
+                ItemPrice = 9,
+                ItemID = 2,
+                description = "Fresh salad with grilled chicken and Caesar dressing",
+                availability = true,
+                category = "Salad",
+                recipe = new List<ItemIngredient>
+                {
+                    new ItemIngredient { ItemIngredientID = 102, ItemIngredientQuantity = "300g" },
+                    new ItemIngredient { ItemIngredientID = 202, ItemIngredientQuantity = "1 head" },
+                    new ItemIngredient { ItemIngredientID = 302, ItemIngredientQuantity = "1 cup" },
+                    new ItemIngredient { ItemIngredientID = 402, ItemIngredientQuantity = "1/4 cup" },
+                    new ItemIngredient { ItemIngredientID = 502, ItemIngredientQuantity = "2 tbsp" }
+                }
+            },
+            new Item
+            {
+
+                ItemName = "Chicken Caesar Salad",
+                ItemPrice = 9,
+                ItemID = 3,
+                description = "Fresh salad with grilled chicken and Caesar dressing",
+                availability = true,
+                category = "Salad",
+                recipe = new List<ItemIngredient>
+                {
+                    new ItemIngredient { ItemIngredientID = 102, ItemIngredientQuantity = "300g" },
+                    new ItemIngredient { ItemIngredientID = 202, ItemIngredientQuantity = "1 head" },
+                    new ItemIngredient { ItemIngredientID = 302, ItemIngredientQuantity = "1 cup" },
+                    new ItemIngredient { ItemIngredientID = 402, ItemIngredientQuantity = "1/4 cup" },
+                    new ItemIngredient { ItemIngredientID = 502, ItemIngredientQuantity = "2 tbsp" }
+                }
+            },
+            new Item
+            {
+
+                ItemName = "Chicken Caesar Salad",
+                ItemPrice = 9,
+                 ItemID = 4,
+                description = "Fresh salad with grilled chicken and Caesar dressing",
+                availability = true,
+                category = "Salad",
+                recipe = new List<ItemIngredient>
+                {
+                    new ItemIngredient { ItemIngredientID = 102, ItemIngredientQuantity = "300g" },
+                    new ItemIngredient { ItemIngredientID = 202, ItemIngredientQuantity = "1 head" },
+                    new ItemIngredient { ItemIngredientID = 302, ItemIngredientQuantity = "1 cup" },
+                    new ItemIngredient { ItemIngredientID = 402, ItemIngredientQuantity = "1/4 cup" },
+                    new ItemIngredient { ItemIngredientID = 502, ItemIngredientQuantity = "2 tbsp" }
+                }
+            }
         };
 
-        private menu(){}
+        private menu() { }
 
         private static menu MyMenu;
 
@@ -35,25 +97,34 @@ namespace round2
         }
         public void showItems()
         {
-            foreach (var x in MenuItems)
+            foreach (var MenuItems in MenuItems)
             {
-                Console.Write($"item name {x.ItemName}  ");
-                Console.Write($"item name {x.ItemPrice}  ");
-                Console.Write($"item name {x.ItemID}  ");
-                Console.Write($"item name {x.description}   ");
+                Console.WriteLine($"item ID           : {MenuItems.ItemID}  ");
+                Console.WriteLine($"item name         : {MenuItems.ItemName}  ");
+                Console.WriteLine($"item price        : {MenuItems.ItemPrice}  ");
+                Console.WriteLine($"item description  : {MenuItems.description}   ");
+                Console.WriteLine($"item availability : {MenuItems.availability}   ");
+                Console.WriteLine($"item category     : {MenuItems.category}   ");
+                Console.WriteLine("Recipe :");
+                foreach (var Ingred in MenuItems.recipe)
+                {
+                    Console.WriteLine($"    Ingredient ID       : {Ingred.ItemIngredientID}");
+                    Console.WriteLine($"    Ingredient Quantity : {Ingred.ItemIngredientQuantity}");
+                }
                 Console.WriteLine();
+                Console.WriteLine("******************************************************************");
             }
         }
-    
-        public void AddItem(string NewItemName,int NewItemPrice ,int NewItemID ,string NewDescription)
-        { 
+
+        public void AddItem(string NewItemName, int NewItemPrice, int NewItemID, string NewDescription)
+        {
             MenuItems.Add(new Item
             { ItemName = NewItemName, ItemPrice = NewItemPrice, ItemID = NewItemID, description = NewDescription });
         }
 
         public void RemoveItem(int deleteItemID)
         {
-           foreach(var x in MenuItems)
+            foreach (var x in MenuItems)
             {
                 if (x.ItemID == deleteItemID)
                 {
@@ -62,4 +133,5 @@ namespace round2
             }
         }
     }
+}
 }
