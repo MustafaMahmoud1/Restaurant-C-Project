34687bfc788e9532532e457dc84b23d0d320ab91
MartinaSaid3 <155316254+MartinaSaid3@users.noreply.github.com>
2024-01-01 19:59:56 +0200

    Add files via upload

---
+++ IOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Project
+{
+    internal interface IOrder
+    {
+        public void CreateOrder(List<int> ItemId, List<int> Quantities);
+        public void ReserveTable(int TableNo, DateTime dateTime);
+    }
+}
