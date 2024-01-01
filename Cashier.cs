b2cb82617ff08c69cb08f8254a9af5b6643bfb52
MartinaSaid3 <155316254+MartinaSaid3@users.noreply.github.com>
2024-01-01 16:36:14 +0200

    Add files via upload

---
+++ Cashier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Project
+{
+    internal class Cashier : Employee
+    {
+        public Cashier(int empId, int empSalary, string empName, string UserName, string UserPassword, string UserRole) : base(empId, empSalary, empName, UserName, UserPassword, "Cashier") { }
+        public void ShowOrder() { }
+        public void PrintInvoice() { }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Project
+{
+    internal abstract class User
+    {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserRole {  get; set; }
+        public User (string UserName, string UserPassword, string UserRole)
+        {
+            this.UserName = UserName;
+            this.UserPassword = UserPassword;
+            this.UserRole = UserRole;
+        }
+        public abstract void SignIn();
+
+    }
+
+}
