67d727c0119c4219ef276c475c859ebbcdd88377
MustafaMahmoud1 <106413978+MustafaMahmoud1@users.noreply.github.com>
2024-01-02 14:52:29 +0200

    .

--- order.cs
+++ order.cs
@@ -9,6 +9,7 @@ namespace Restaurant_C__Project
 {
     public class Order
     {
+<<<<<<< HEAD
         public int orderid { get; set; }
         public int OrderPrice { get; set; }
 
@@ -19,20 +20,44 @@ namespace Restaurant_C__Project
             listitem = null;
             OrderPrice = 0;
           
+=======
+        public int OrderId { get; set; }
+
+        
+
+        public List<int> ListItem { get; } = new List<int>();
+        public order()
+        {
+            OrderId = 0;
+            ListItem = null;
+          
+>>>>>>> 38d97c139af3fba3f5784c3233cbe3e9c82b4e98
         }
       
         public void AddToOrder()
         {
+<<<<<<< HEAD
+            foreach (var item in listitem)
+            {
+                listitem.Add(item);
+=======
             foreach (var item in ListItem)
             {
                 ListItem.Add(item);
+>>>>>>> 38d97c139af3fba3f5784c3233cbe3e9c82b4e98
             }
         }
         public void RemoveFromOrder()
         {
+<<<<<<< HEAD
+            foreach (var item in listitem)
+            {
+                listitem.Remove(item);
+=======
             foreach (var item in ListItem)
             {
                 ListItem.Remove(item);
+>>>>>>> 38d97c139af3fba3f5784c3233cbe3e9c82b4e98
             }
         }
         public void DeleteOrder()
@@ -41,7 +66,11 @@ namespace Restaurant_C__Project
         }
         public void ShowOrder()
         {
+<<<<<<< HEAD
+            foreach (var item in listitem)
+=======
             foreach (var item in ListItem)
+>>>>>>> 38d97c139af3fba3f5784c3233cbe3e9c82b4e98
             {
                 Console.WriteLine(item);
             }
