000dae786ef5d6d6ffccd82cf77fb3879668d8c4
abdelrahman shalaby <abdelrahman shalaby@LAPTOP-4GRETQ6T>
2024-01-03 19:38:05 +0200

    commit

--- ingredients.cs
+++ ingredients.cs
@@ -17,18 +17,6 @@ namespace Restaurant_C__Project
         public bool IngredientStatus { get; set; }
         public int IngredientQuantity { get; set; }
 
-        /// <summary>
-        /// JSON PART
-        /// </summary>
-        /// 
-
-
-        //public ingredients() { }
-        //static List<ingredients> LoadIngredients(string filePath)
-        //{
-        //    string jsonText = System.IO.File.ReadAllText(filePath);
-        //    return JsonConvert.DeserializeObject<List<ingredients>>(jsonText);
-        //}
         //public void LoadIngredient()
         //{
         //    List<ingredients> LoadIngredients = new List<ingredients>();
@@ -46,13 +34,6 @@ namespace Restaurant_C__Project
                 Console.WriteLine(item);
             }
         }
-        //foreach (var x in Deserialized)
-        //{
-        //    Console.WriteLine(x.IngredientName);
-        //    Console.WriteLine(x.IngredientID);
-        //    Console.WriteLine(x.IngredientStatus);
-        //    Console.WriteLine(x.IngredientQuantity);
-        //}
     }
 
 
