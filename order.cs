6e46364b5103cd848833a2be5507169c4d3d28cc
MustafaMahmoud1 <106413978+MustafaMahmoud1@users.noreply.github.com>
2024-01-13 03:43:55 +0200

    .

--- order.cs
+++ order.cs
@@ -17,72 +17,60 @@ namespace Restaurant_C__Project
         public static List<Order> WaitingOrderList = new List<Order>();
 
         public int OrderId { get; set; }
-
         public double OrderPrice { get; set; }
-
         public string CustomerId { get; set; }
 
-        public List<OrderedItem> ListItem { get; } = new List<OrderedItem>();
-        public Order()
+        public List<OrderedItem> ListItem { get; set; } = new List<OrderedItem>();
+        public Order(int orderPrice, string customerid, List<OrderedItem> items)
         {
-            OrderId = 0;
-            ListItem = null;
-
+            Random rnd = new Random();
+            OrderId = rnd.Next(1, 10000000);
+            OrderPrice = orderPrice;
+            CustomerId = customerid;
+            ListItem = items;
         }
 
-
-        //Parametarize constructor
-        public Order(List<OrderedItem> Items)
-        {
-            Random R = new Random();
-            OrderId = R.Next();
-            //OrderId = OrderId;
-            ListItem = Items;
-            double TotalPrice = 0.0;
-            foreach (Order OrderItem x in ListItem)
-            {
-                TotalPrice += (double)(x.Quantity * x.Price);
-            }
-            OrderPrice = TotalPrice;
-        }
         //function to create order
-        public static void CreateOrder()
-        {
-            Menu.GetInstance().LoadAllItemsFromJson(@"C:\Users\abdelrahman shalaby\Source\Repos\MustafaMahmoud1\Restaurant-C-Project\Menu.json");
-            Menu.GetInstance().Showitemstocustomer();
-            Console.WriteLine("Please chosse Item Id");
-            int Item = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please Inter The Quentity");
-            int Quentity = int.Parse(Console.ReadLine());
-            foreach (Item item in Menu.GetInstance().AllItems)
-            {
-                if (item.ItemID == Item)
-                {
-                    OrderItem orderitem = new OrderItem(Item, Quentity, item.ItemPrice);
-                    ListItem.Add(orderitem);
-                }
-            }
-            Order order = new Order(ListItem);
-            ShowOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
-            ShowActiveOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-            AddToOrderList(order);
-            Console.WriteLine("Order Created Successfully");
-            SaveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
-            SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-        }
+        //public static void CreateOrder()
+        //{
+        //    Menu.GetInstance().LoadAllItemsFromJson(@"C:\Users\abdelrahman shalaby\Source\Repos\MustafaMahmoud1\Restaurant-C-Project\Menu.json");
+        //    Menu.GetInstance().Showitemstocustomer();
+        //    Console.WriteLine("Please chosse Item Id");
+        //    int Item = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Please Inter The Quentity");
+        //    int Quentity = int.Parse(Console.ReadLine());
+        //    foreach (Item item in Menu.GetInstance().AllItems)
+        //    {
+        //        if (item.ItemID == Item)
+        //        {
+        //            OrderItem orderitem = new OrderItem(Item, Quentity, item.ItemPrice);
+        //            ListItem.Add(orderitem);
+        //        }
+        //    }
+        //    Order order = new Order(ListItem);
+        //    ShowOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
+        //    ShowActiveOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
+        //    AddToOrderList(order);
+        //    Console.WriteLine("Order Created Successfully");
+        //    SaveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
+        //    SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
+        //}
 
         public static void AddToOrderList(Order order)
         {
             if (ActiveOderList.Count < 10)
             {
+                LoadAllOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
+                LoadAllActiveOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
                 OrderList.Add(order);
                 ActiveOderList.Add(order);
+                SaveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
+                SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
                 return;
             }
             else
             {
-                ShowWaitingOrderList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
-
+                LoadAllWaitingOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
                 WaitingOrderList.Add(order);
                 SaveOrderToWaitingJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
             }
@@ -90,84 +78,78 @@ namespace Restaurant_C__Project
         }
 
 
-        //load active orders from json
-        public static void ShowActiveOrdersList(string JsonFile)
-        {
-            string json = File.ReadAllText(JsonFile);
-            ActiveOderList = JsonConvert.DeserializeObject<List<Order>>(json);
 
-        }
         //function to show all Active Orders
-        public static void LoadAllActiveOrder()
-        {
-            Console.WriteLine("All Active Oders Oders ");
-            foreach (var x in ActiveOderList)
-            {
-                Console.WriteLine("Oder Id:   " + x.OrderId);
-                foreach (OrderItem y in x.ListItem)
-                {
-                    Console.WriteLine("Item Id:   " + y.ItemId);
-                    Console.WriteLine("Item Quentity:   " + y.Quantity);
-                    Console.WriteLine("Item Price:   " + y.Price);
-                }
-                Console.WriteLine("Order Total Price:   " + x.OrderPrice);
+        //public static void LoadAllActiveOrder()
+        //{
+        //    Console.WriteLine("All Active Oders Oders ");
+        //    foreach (var x in ActiveOderList)
+        //    {
+        //        Console.WriteLine("Oder Id:   " + x.OrderId);
+        //        foreach (OrderItem y in x.ListItem)
+        //        {
+        //            Console.WriteLine("Item Id:   " + y.ItemId);
+        //            Console.WriteLine("Item Quentity:   " + y.Quantity);
+        //            Console.WriteLine("Item Price:   " + y.Price);
+        //        }
+        //        Console.WriteLine("Order Total Price:   " + x.OrderPrice);
 
-            }
-        }
-        public static void ShowWaitingOrderList(string JsonFile)
-        {
-            WebRequest reque = WebRequest.Create(JsonFile);
-            WebResponse respon = reque.GetResponse();
-            using Stream datastre = respon.GetResponseStream();
-            StreamReader read = new StreamReader(datastre);
-            string responsefromserv = read.ReadToEnd();
-            WaitingOrderList = JsonConvert.DeserializeObject<List<Order>>(responsefromserv);
+        //    }
+        //}
+        //public static void ShowWaitingOrderList(string JsonFile)
+        //{
+        //    WebRequest reque = WebRequest.Create(JsonFile);
+        //    WebResponse respon = reque.GetResponse();
+        //    using Stream datastre = respon.GetResponseStream();
+        //    StreamReader read = new StreamReader(datastre);
+        //    string responsefromserv = read.ReadToEnd();
+        //    WaitingOrderList = JsonConvert.DeserializeObject<List<Order>>(responsefromserv);
 
-        }
+        //}
         ////////
-        public static void ShowOrdersList(string JsonFile)
-        {
-            WebRequest request = WebRequest.Create(JsonFile);
-            WebResponse response = request.GetResponse();
-            using Stream datastream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(datastream);
-            string responsefromserver = reader.ReadToEnd();
-            OrderList = JsonConvert.DeserializeObject<List<Order>>(responsefromserver);
-            foreach (Order x in OrderList)
-            {
-                Console.WriteLine(x.OrderId);
-                Console.WriteLine(x.OrderPrice);
-                Console.WriteLine(x.CustomerId);
-                foreach (OrderItem y in x.ListItem)
-                {
-                    Console.WriteLine(y.ItemId);
-                    Console.WriteLine(y.Quantity);
-                    Console.WriteLine(y.Price);
-                }
-            }
+        //public static void ShowOrdersList(string JsonFile)
+        //{
+        //    WebRequest request = WebRequest.Create(JsonFile);
+        //    WebResponse response = request.GetResponse();
+        //    using Stream datastream = response.GetResponseStream();
+        //    StreamReader reader = new StreamReader(datastream);
+        //    string responsefromserver = reader.ReadToEnd();
+        //    OrderList = JsonConvert.DeserializeObject<List<Order>>(responsefromserver);
+        //    foreach (Order x in OrderList)
+        //    {
+        //        Console.WriteLine(x.OrderId);
+        //        Console.WriteLine(x.OrderPrice);
+        //        Console.WriteLine(x.CustomerId);
+        //        foreach (OrderItem y in x.ListItem)
+        //        {
+        //            Console.WriteLine(y.ItemId);
+        //            Console.WriteLine(y.Quantity);
+        //            Console.WriteLine(y.Price);
+        //        }
+        //    }
 
-        }
+        //}
 
-        public static void RemoveItemFromOrder(int OrderId, int ItemId)      
-        {
-            foreach (Order x in OrderList)
-            {
-                if (x.OrderId == OrderId)
-                {
-                    foreach (OrderItem y in x.ListItem)
-                    {
-                        if (y.ItemId == ItemId)
-                        {
-                            ListItem.Remove(y);
-                            Console.WriteLine("sucssesfully removed");
-                            x.OrderPrice -= y.Price * y.Quantity;
-                            Console.WriteLine(x.OrderPrice);
-                        }
-                    }
-                }
-                
-            }
-        }
+        //public static void RemoveItemFromOrder(int OrderId, int ItemId)
+        //{
+        //    foreach (Order x in OrderList)
+        //    {
+        //        if (x.OrderId == OrderId)
+        //        {
+        //            foreach (OrderItem y in x.ListItem)
+        //            {
+        //                if (y.ItemId == ItemId)
+        //                {
+        //                    ListItem.Remove(y);
+        //                    Console.WriteLine("sucssesfully removed");
+        //                    x.OrderPrice -= y.Price * y.Quantity;
+        //                    Console.WriteLine(x.OrderPrice);
+        //                }
+        //            }
+        //        }
+
+        //    }
+        //}
 
 
         public static void ClearOrderList()
@@ -213,50 +195,32 @@ namespace Restaurant_C__Project
      
  }*/
         //Remove Order from active orders then take waiting orders if exist
-        public static void RemoveFromActiveList(int OrderId)
+        public static void FinishAnOrder(int OrderId)
         {
-
-            ShowActiveOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-
+            LoadAllActiveOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
+            LoadAllOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
             foreach (Order order in ActiveOderList)
             {
                 if (order.OrderId == OrderId)
                 {
                     ActiveOderList.Remove(order);
-                    SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-
-
-                    ShowWaitingOrderList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
-                    ShowActiveOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-                    ShowOrdersList(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
-
-                    if (WaitingOrderList != null)
+                    OrderList.Remove(order);
+                    LoadAllWaitingOrders(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
+                    if (WaitingOrderList.Count != 0)
                     {
-                        for (int j = 0; j < WaitingOrderList.Count; j++)
-                        {
-                            if (ActiveOderList.Count < 10)
-                            {
-
-
-                                AddToOrderList(WaitingOrderList[j]);
-                                WaitingOrderList.Remove(WaitingOrderList[j]);
-                                Console.WriteLine("New Order From Waiting List");
-                                SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
-
-                                SaveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
-                                SaveOrderToWaitingJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
-                            }
-
-                        }
-                        //ActiveOderList[ActiveOderList.Count - 1] = WaitingOrderList[0];
-
-                        Console.WriteLine("Remove done succseefully");
+                        ActiveOderList.Add(WaitingOrderList[0]);
+                        OrderList.Add(WaitingOrderList[0]);
+                        WaitingOrderList.Remove(WaitingOrderList[0]);
                     }
-
-
-
-
+                    SaveActiveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\ActiveOrder.json");
+                    SaveOrderToJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\Order.json");
+                    SaveOrderToWaitingJson(@"C:\\Users\\M&M\\Source\\Repos\\Restaurant-C-Project\\WaitingOrders.json");
+                    return;
                 }
+
+
+
+
             }
         }
         //--------------save all listes to jsons files-------------------//
@@ -278,9 +242,80 @@ namespace Restaurant_C__Project
             string json = JsonConvert.SerializeObject(WaitingOrderList, Formatting.Indented);
             File.WriteAllText(JsonFile, json);
         }
+        //4-Load all orders from json
+        public static void LoadAllOrders(string JsonFile)
+        {
+            string json = File.ReadAllText(JsonFile);
+            OrderList = JsonConvert.DeserializeObject<List<Order>>(json);
+        }
+        //5-Load all waiting orders from json
+        public static void LoadAllWaitingOrders(string JsonFile)
+        {
+            string json = File.ReadAllText(JsonFile);
+            WaitingOrderList = JsonConvert.DeserializeObject<List<Order>>(json);
+        }
+        //6-Load all active orders from json
+        public static void LoadAllActiveOrders(string JsonFile)
+        {
+            string json = File.ReadAllText(JsonFile);
+            ActiveOderList = JsonConvert.DeserializeObject<List<Order>>(json);
+        }
 
-
+        /// <summary>
+        /// //////////////////////////////////////////////////
+        /// </summary>
+        
+        public static void ShowOrdersList()
+        {
+            foreach (var Order in OrderList)
+            {
+                Console.WriteLine($"Order ID : {Order.OrderId}");
+                Console.WriteLine($"Order Price : {Order.OrderPrice}");
+                Console.WriteLine($"Customer ID : {Order.CustomerId}");
+                Console.WriteLine("Items :");
+                foreach (var Item in Order.ListItem)
+                {
+                    Console.WriteLine($"Item ID : {Item.ItemID}");
+                    Console.WriteLine($"Item Price : {Item.Price}");
+                    Console.WriteLine($"Item Quantity : {Item.Quantity}");
+                }
+                Console.WriteLine("******************************************************************");
+            }
+        }
+        public static void ShowActiveOrdersList()
+        {
+            foreach (var Order in ActiveOderList)
+            {
+                Console.WriteLine($"Order ID : {Order.OrderId}");
+                Console.WriteLine($"Order Price : {Order.OrderPrice}");
+                Console.WriteLine($"Customer ID : {Order.CustomerId}");
+                Console.WriteLine("Items :");
+                foreach (var Item in Order.ListItem)
+                {
+                    Console.WriteLine($"Item ID : {Item.ItemID}");
+                    Console.WriteLine($"Item Price : {Item.Price}");
+                    Console.WriteLine($"Item Quantity : {Item.Quantity}");
+                }
+                Console.WriteLine("******************************************************************");
+            }
+        }
+        public static void ShowWaitingOrdersList()
+        {
+            foreach (var Order in WaitingOrderList)
+            {
+                Console.WriteLine($"Order ID : {Order.OrderId}");
+                Console.WriteLine($"Order Price : {Order.OrderPrice}");
+                Console.WriteLine($"Customer ID : {Order.CustomerId}");
+                Console.WriteLine("Items :");
+                foreach (var Item in Order.ListItem)
+                {
+                    Console.WriteLine($"Item ID : {Item.ItemID}");
+                    Console.WriteLine($"Item Price : {Item.Price}");
+                    Console.WriteLine($"Item Quantity : {Item.Quantity}");
+                }
+                Console.WriteLine("******************************************************************");
+            }
+        }
 
     }
-
 }
